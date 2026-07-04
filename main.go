ed67e3bc3ba5925e8eb35dea405a2f133c59543a
tuken <tuken@gmail.com>
2026-07-04 17:10:16 +0900

    update: main.go
    routerのを使用するように

--- main.go
+++ main.go
@@ -7,33 +7,37 @@ import (
 	"os/signal"
 	"syscall"
 
-	mqtt "github.com/eclipse/paho.mqtt.golang"
-	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 	myctx "github.com/tuken/triton/context"
 	"github.com/tuken/triton/logger"
-	myser "github.com/tuken/triton/serial"
+	"github.com/tuken/triton/router"
 	"github.com/tuken/triton/usb"
 	"go.bug.st/serial"
 )
 
 func main() {
 
+	// Ctrl-C / SIGTERM が来ると ctx がキャンセルされる
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
 	log := logger.NewLogger()
-	ctx := context.WithValue(context.Background(), myctx.LoggerKey, log)
+	ctx = context.WithValue(ctx, myctx.LoggerKey, log)
 
 	watch := usb.NewWatch(ctx)
 
-	port := watch.Find("BraveJIG Router")
-	if port != "" {
-		fmt.Println("既に接続されている:", port)
-	}
+	portName := watch.Find("BraveJIG Router")
+	if portName == "" {
 
-	com := &myser.Com{}
+		fmt.Println("未接続:", portName)
+		return
+	}
 
 	log.Infow("アプリケーション起動")
 
+	com := router.NewCom()
+
 	// COMポートをオープン
-	if err := com.Connect(ctx, "/dev/ttyACM0", &serial.Mode{
+	if err := com.Connect(portName, &serial.Mode{
 		BaudRate: 115200,
 		DataBits: 8,
 		Parity:   serial.NoParity,
@@ -45,49 +49,5 @@ func main() {
 
 	defer com.Disconnect()
 
-	paired, err := com.WaitKeepAliveRequest()
-	if err != nil {
-		log.Fatalw("KeepAlive要求待機エラー", "error", err)
-		return
-	}
-
-	if paired {
-		log.Infow("初めての接続")
-	}
-
-	// InfluxDB 接続
-	app.influx = influxdb2.NewClient(cfg.InfluxDBURL, cfg.InfluxDBToken)
-	app.writeAPI = app.influx.WriteAPIBlocking(cfg.InfluxDBOrg, cfg.InfluxDBBucket)
-
-	// MQTT 接続
-	opts := mqtt.NewClientOptions()
-	opts.AddBroker(fmt.Sprintf("tcp://%s:%d", cfg.MQTTBroker, cfg.MQTTPort))
-	opts.SetAutoReconnect(true)
-	opts.OnConnect = func(client mqtt.Client) {
-		log.Infow("[MQTT] 接続成功")
-		client.Subscribe("bravejig/cmd/interval", 0, app.onIntervalCommand)
-		client.Subscribe("bravejig/cmd/send_now", 0, app.onSendNowCommand)
-		log.Printf("[MQTT] 購読完了: bravejig/cmd/interval, bravejig/cmd/send_now")
-	}
-	opts.OnConnectionLost = func(client mqtt.Client, err error) {
-		log.Printf("[MQTT] 切断: %v", err)
-	}
-	app.mqttClient = mqtt.NewClient(opts)
-	if token := app.mqttClient.Connect(); token.Wait() && token.Error() != nil {
-		log.Fatalf("[MQTT] 接続失敗: %v", token.Error())
-	}
-	log.Printf("[MQTT] クライアント起動")
-
-	// シグナルハンドリング
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
-	go func() {
-		<-sig
-		log.Printf("終了")
-		app.mqttClient.Disconnect(250)
-		app.influx.Close()
-		os.Exit(0)
-	}()
-
-	app.run()
+	com.Run(ctx)
 }
