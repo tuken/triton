7d4f7f584b109e43ff1f05a4271ceb99ea959431
tuken <tuken@gmail.com>
2026-07-20 14:27:35 +0900

    update: serial/packet.go
    UplinkNotifyを削除（独自ファイルに移動）

--- serial/packet.go
+++ serial/packet.go
@@ -4,6 +4,8 @@ import (
 	"encoding/binary"
 	"fmt"
 	"io"
+
+	"github.com/tuken/triton/serial/packet"
 )
 
 const (
@@ -48,7 +50,7 @@ func newPacketByType(typ byte) (Packet, error) {
 	switch typ {
 
 	case TypeUplinkNotify:
-		return &UplinkNotify{}, nil
+		return &packet.UplinkNotify{}, nil
 
 	case TypeDownlinkResponse:
 		return &DownlinkResponse{}, nil
@@ -234,56 +236,6 @@ const (
 	ResultBusyRouter         DownlinkResult = 0x08
 	ResultBusyModule         DownlinkResult = 0x09
 )
-
-// UplinkNotify アップリンク通知パケット（可変長、DataLengthで指定される）
-type UplinkNotify struct {
-	ProtocolVersion byte   // Index 0: 0x01
-	Type            byte   // Index 1: 0x00
-	DataLength      uint16 // Index 2-3: データ長（0..65535）
-	UnixTime        uint32 // Index 4-7: Little Endian
-	DeviceID        uint64 // Index 8-15: Little Endian
-	SensorID        uint16 // Index 16-17: Little Endian
-	Rssi            byte   // Index 18: RSSI値（-128..127）
-	SequenceNo      uint16 // Index 19-20: Little Endian
-	Data            []byte // Index 21-: 可変長データ（DataLength バイト）
-}
-
-func (p *UplinkNotify) Unmarshal(buf []byte) error {
-
-	if len(buf) < 21 {
-		return fmt.Errorf("too short: %d bytes", len(buf))
-	}
-
-	p.ProtocolVersion = buf[0]
-	p.Type = buf[1]
-	p.DataLength = binary.LittleEndian.Uint16(buf[2:4])
-	p.UnixTime = binary.LittleEndian.Uint32(buf[4:8])
-	p.DeviceID = binary.LittleEndian.Uint64(buf[8:16])
-	p.SensorID = binary.LittleEndian.Uint16(buf[16:18])
-	p.Rssi = buf[18]
-	p.SequenceNo = binary.LittleEndian.Uint16(buf[19:21])
-
-	if len(buf) >= 21+int(p.DataLength) {
-		p.Data = append([]byte(nil), buf[21:21+int(p.DataLength)]...)
-	}
-
-	return nil
-}
-
-func (p *UplinkNotify) FixedSize() int {
-
-	return 21
-}
-
-func (p *UplinkNotify) VariableSize(fixed []byte) int {
-
-	// DataLength は固定部 Index 2-3（Little Endian）にある
-	if len(fixed) < 4 {
-		return 0
-	}
-
-	return int(binary.LittleEndian.Uint16(fixed[2:4]))
-}
 
 // DownlinkResponse ダウンリンク応答パケット（20バイト固定）
 type DownlinkResponse struct {
